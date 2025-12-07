748f2250db61f51c7542bb81ea809d284bdf7a3c
ulise <ulise@DRIMCS>
2025-12-07 16:13:34 -0600

    Agregar archivos de proyecto.

---
+++ SharpFiles/MyHelpers/FileManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpFiles.MyHelpers
+{
+    public static class FileManager
+    {
+
+        public static IEnumerable<FileInfo> GetAllFiles(string rutaPadre, string tipoExtension)
+        {
+            IEnumerable<FileInfo> files;
+            if (tipoExtension.Contains("None"))
+            {
+                files = Directory.GetFiles(rutaPadre, "*", SearchOption.AllDirectories)
+                        .Select(arch => new FileInfo(arch)).OrderByDescending(arch => arch.LastWriteTime);
+
+            }
+            else
+            {
+                files = Directory.GetFiles(rutaPadre, $"*.{tipoExtension}", SearchOption.AllDirectories)
+                        .Select(arch => new FileInfo(arch)).OrderByDescending(arch => arch.LastWriteTime);
+
+            }
+            return files.Where(f => f != null && !string.IsNullOrWhiteSpace(f.Name));
+        }
+
+        public static async Task<IEnumerable<FileInfo>> GetAllFilesAsync(string rutaPadre, string tipoExtension)
+        {
+            return await Task.Run(() => GetAllFiles(rutaPadre, tipoExtension));
+        }
+
+        public static IEnumerable<string> GetExtension()
+        {
+            var listaExtension = Enum.GetValues(typeof(ExtensionesArchivos))
+                .Cast<ExtensionesArchivos>().Select(e => e.ToString().Replace("_", ""));
+
+            return listaExtension;
+        }
+
+    }
+    enum ExtensionesArchivos
+    {
+        None,
+        //Imagenes
+        jpg, png, gif, bmp, tiff, jpeg, webp,
+        //Textos
+        txt, pdf, doc, docx,
+        //Hojas de calculo
+        xls, xlsx, ods,
+        // Presentaciones
+        ppt, pptx, odp,
+        // Archivos comprimidos
+        zip, rar, cbr, cbz,
+        // Archivos de video
+        mp4, avi, mkv, mov, wmv, flv, webm, mpg, mpeg, _3gp,
+        // Archivos de audio
+        mp3, wav, flac, aac, ogg, m4a,
+        // Archivos de base de datos
+        sql, db, sqlite, mdf
+    }
+}
---
+++ SharpFiles/ViewModels/ShowFilesViewModels.cs
@@ -0,0 +1,135 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using SharpFiles.MyHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SharpFiles.ViewModels
+{
+    public partial class ShowFilesViewModels : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<FileInfo> _files = new ObservableCollection<FileInfo>();
+
+        [ObservableProperty]
+        List<string> _fileExtensions = new List<string>();
+
+        [ObservableProperty]
+        private string? _projectTitle;
+
+        [ObservableProperty]
+        private string? _rutaCarpeta = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+        [ObservableProperty]
+        private string? _selectedExtension;
+
+        [ObservableProperty]
+        private string? _fileName;
+
+        [ObservableProperty]
+        private int _numFiles;
+
+        [ObservableProperty]
+        private FileInfo? _archivoSeleccionado;
+
+        public ShowFilesViewModels()
+        {
+            ProjectTitle = "Buscador de archivos";
+            GetFileExtensions();
+            
+        }
+
+        partial void OnSelectedExtensionChanged(string? value)
+        {
+            SearchFilesCommand.NotifyCanExecuteChanged();
+        }
+
+        partial void OnRutaCarpetaChanged(string? value)
+        {
+            SearchFilesCommand.NotifyCanExecuteChanged();
+        }
+
+        [RelayCommand]
+        private void SearchFolder()
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                InitialDirectory = @"C:",
+                Title = "Seleccionar carpeta donde se moveran los archivos",
+                Filter = "Carpetas|*.none",
+                CheckFileExists = false,
+                CheckPathExists = true,
+                FileName = "Selecciona tu carpeta"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                RutaCarpeta = Path.GetDirectoryName(dialog.FileName);
+            }
+        }
+
+        [RelayCommand(CanExecute = nameof(CanShowFiles))]
+        private async Task SearchFiles()
+        {
+            
+            if (!string.IsNullOrWhiteSpace(RutaCarpeta) && !string.IsNullOrWhiteSpace(SelectedExtension))
+            {
+                var files = await FileManager.GetAllFilesAsync(RutaCarpeta, SelectedExtension);
+                Files.Clear();
+                foreach (var file in files)
+                {
+                    Files.Add(file);
+                }
+                NumFiles = Files.Count;
+
+            }
+        }
+
+        private void GetFileExtensions()
+        {
+            foreach (var item in FileManager.GetExtension())
+            {
+                FileExtensions.Add(item);
+            }
+        }
+
+
+        private bool CanShowFiles()
+        {
+            return (!string.IsNullOrWhiteSpace(SelectedExtension) && !string.IsNullOrWhiteSpace(RutaCarpeta));
+        }
+
+
+        
+
+
+        //[RelayCommand]
+        //private void ShowFolder()
+        //{
+        //    Process.Start("explorer.exe", RutaCarpeta);
+        //}
+
+        [RelayCommand]
+        private void ItemFile(FileInfo fileInfo)
+        {
+            ArchivoSeleccionado = fileInfo;
+
+            //MessageBox.Show($"Este es el archivo con el nombre {ArchivoSeleccionado.Name}", "informacion de una archivo seleccionado", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            var rutaArchivo = ArchivoSeleccionado.FullName;
+            Process.Start("explorer.exe", "/select,\"" + rutaArchivo + "\"");
+        }
+
+        
+
+    }
+}
